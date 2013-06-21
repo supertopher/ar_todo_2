ee046960466640b3d88a5963ad2053bb5cd50473
Apprentice <apprentice@devbootcamp.com>
2013-06-21 14:55:09 -0700

    Added ARGV skeleton code

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,10 @@ require_relative '../../config/application'
 
 class Task < ActiveRecord::Base
   has_one :list
+
+  def mark_completed
+    
+  end
 end
 
 Task.create(description: "Give two doses of happy powder to Chris")
--- input.rb
+++ input.rb
@@ -4,5 +4,12 @@ require_relative '/app/models/task.rb'
 
 case ARGV[0]
 when "list"
-  list_view.list
+  puts Task.all
+when "add"
+  Task.create!(description: ARGV[1..-1])
+when "remove" || "delete"
+  Task.delete(ARGV[1])
+  puts "Removed task at index #{ARGV[1]}."
+when "bobby_tables"
+  puts "drop the table"
 end
